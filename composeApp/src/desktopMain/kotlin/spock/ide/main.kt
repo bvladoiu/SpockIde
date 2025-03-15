1ecf9f0a5bed26961b3556d20e4304e85665ed07
bvladoiu <bogdan.vladoiu.lbs@gmail.com>
2025-03-15 02:40:28 +0200

    added app dirs

---
+++ composeApp/src/desktopMain/kotlin/spock/ide/main.kt
@@ -0,0 +1,13 @@
+package spock.ide
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "KotlinProject",
+    ) {
+        App()
+    }
+}
